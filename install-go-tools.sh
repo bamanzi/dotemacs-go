29f7fc080d8f8e46a7d860ae1abef152e9ca46c0
Ba Manzi <ba.manzi@yahoo.com>
2023-01-29 16:02:20 +0800

    add install script & some docs for gopls server

--- install-go-tools.sh
+++ install-go-tools.sh
@@ -11,6 +11,15 @@ go install golang.org/x/tools/cmd/guru
 # * delve debugger
 # https://github.com/go-delve/delve
 go install github.com/go-delve/delve/cmd/dlv@latest
-# go1.14 -> dlv@1.6.1
+## go1.14 -> dlv@1.6.1
 # go install github.com/go-delve/delve/cmd/dlv@v1.6.1
 
+
+# * LSP server
+# https://github.com/golang/tools/blob/master/gopls/doc/emacs.md
+# NOTE: gopls _offically_ supports the last 2 major Go releases,
+#       and attempts to support for the last 4 jaor Go releases
+#       https://github.com/golang/tools/blob/master/gopls/README.md#support-policy
+go install golang.org/x/tools/gopls@latest
+
+# elisp client: https://github.com/joaotavora/eglot
